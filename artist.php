190df3cfcc7c205c186124501856ad755806fc2a
camille <camille_chen25@hotmail.fr>
2019-11-14 17:51:15 +0100

    update page artiste

--- artist.php
+++ artist.php
@@ -5,15 +5,14 @@
     <?php include('layout/header.php') ?>
     <div id="content">
         <div id="festival">
-            <ul>
-                    <li>
-                        <!-- <img v-bind:src="artist.acf.photo.url"/> -->
-                        <div class="name">nom artiste</div>  
-                        <div class="name">description artiste</div> 
-                       <a href="https://www.digitick.com/festival-tickets"><button type="button" class="btn btn-warning">Billeterie</button></a>
-                    </li>
-                </a>
-            </ul>
+           
+            <!-- <img v-bind:src="artist.acf.photo.url"/> -->
+            <h1>nom artiste</h1>  
+            <div class="name">description artiste</div> 
+              <div class="buttons">  
+                <a href="https://www.digitick.com/festival-tickets"><button type="button" class="btn btn-warning">Billets</button></a>
+                <a href="https://www.digitick.com/festival-tickets"><button type="button" class="btn btn-warning">Partagez</button></a>
+              </div>             
         </div>
         <?php include('script/api.php') ?>
     </div>
