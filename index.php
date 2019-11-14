12a75f002cac73e03089181e630f9bd540421149
saro <cyn.saro@gmail.com>
2019-11-14 15:59:46 +0100

    change API URL

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script>
 
-            var apiURL = 'https://nation-sounds.000webhostapp.com/wp-json/wp/v2/posts?categories=2'
+            var apiURL = 'https://nation-sounds.planethoster.world/wp-json/wp/v2/posts?categories=2'
 
             var festival = new Vue({
                 el: '#festival',
