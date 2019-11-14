ef42906d2a8d6ab7e5c0f504aa57cfeb1b1d6a19
saro <cyn.saro@gmail.com>
2019-11-14 17:09:04 +0100

    Home page : show artists photo

--- index.php
+++ index.php
@@ -6,22 +6,22 @@
 <body>
 
     <div id="content">
-    <?php include("layout/header.php");?>
+     <?php include("layout/header.php");?>
         <div id="festival">
-            <ul  v-if="festivalprogram.length > 0">
-                <a href="">
-                    <li v-for="artist in festivalprogram">
-                        <img v-bind:src="artist.acf.photo.url"/>
-                        <div class="name">{{artist.acf.scene}}</div>  
-                    </li>
-                </a>
+            <ul v-if="festivalprogram.length > 0">
+                <li v-for="artist in festivalprogram" class="homeArtists">
+                    <a v-bind:href="artist.link" >
+                        <img v-bind:src="artist.acf.photo.url" class="pictureArtists"/>
+                        <div class="nameArtists">{{artist.acf.nom}}</div>  
+                    </a>    
+                </li>
             </ul>
         </div>
 
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
-        <!--Boostar-->
+        <!--Boostrap-->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
